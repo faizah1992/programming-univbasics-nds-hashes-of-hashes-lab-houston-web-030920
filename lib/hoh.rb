63097fb6df69a1dea130cf9b813a7a5501815fdc
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-25 22:24:53 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,6 +43,10 @@ def naming_system
           label: "Family",
           sub_category: {
             label: "Genus",
+            sub_category: {
+              label: "Species",
+              sub_category: nil
+            }
           }
         }
       }
